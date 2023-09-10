f7d152863918dcb2fad7fc19411a2e79302aa07e
MRadityaAdhirajasa <radityaadhirajasaa@gmail.com>
2023-09-10 20:38:33 +0700

    prak 2

---
+++ app/Views/profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+<table>
+  <tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td><?= $nama ?></td>
+  </tr>
+  <tr>
+    <td>kelas</td>
+    <td>:</td>
+    <td><?= $kelas ?></td>
+  </tr>
+  <tr>
+    <td>npm</td>
+    <td>:</td>
+    <td><?= $npm ?></td>
+  </tr>
+</table>
+</body>
+</html>
