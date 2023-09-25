7cb17d6eb174b319d139c3d79a0399cadf72e4fa
MRadityaAdhirajasa <radityaadhirajasaa@gmail.com>
2023-09-25 19:36:58 +0700

    prak 4

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,16 +3,16 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use codeigniter\Exceptions\PageNotFoundException;
-
+use App\Models\UserModel;
 
 class UserController extends BaseController
 {
+    protected $helpers = ['form']; 
+
     public function index()
     {
         //
     }
-
 
     public function profile($nama = "", $kelas = "", $npm = "")
     {
@@ -25,12 +25,52 @@ class UserController extends BaseController
         return view('profile', $data);
     }
 
-    public function create(){
-        return view ('create_user');
+    public function create()
+    {   
+        $kelas = [
+            [
+            'id' => 1,
+            'nama_kelas' => 'A'
+            ],
+            [
+                'id' => 2,
+                'nama_kelas' => 'B'
+            ],
+            [
+                'id' => 3,
+                'nama_kelas' => 'C'
+            ],
+            [
+                'id' => 4,
+                'nama_kelas' => 'D'
+                ],
+        ];
+
+        $data = [
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data);
     }
 
-    public function store(){
+    public function store()
+    {
         // dd($this->request->getVar());
+
+        $userModel = new UserModel();
+
+        if (!$this->validate($userModel->getValidationRules())) {
+            session()->setFlashdata('errors', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        }
+
+
+        $userModel->saveUser([
+            'nama' => $this->request->getVar('nama'),
+            'id_kelas' => $this->request->getVar('kelas'),
+            'npm' => $this->request->getVar('npm'),
+        ]);
+
 
         $data = [
             'nama' => $this->request->getVar('nama'),
